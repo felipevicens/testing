d0b11e8bd343a4686377f1200c9644cc27d2df5e
luishens01 <38247377+luishens01@users.noreply.github.com>
2018-05-03 16:29:09 +0200

    New version of upload_package
    New version of upload_package

--- tavern_testing/upload_package.sh
+++ tavern_testing/upload_package.sh
@@ -1,20 +1,76 @@
 #!/bin/bash
 
-PKG=$1
-URL=$2
+#PKG=$1
+#URL=$2
+
+PKG=""
+URL=""
+usage="\nTo use this script you must specify the following options:\n\n-p: path to the package\n-u: url to the api \n\nExample:\n\nsh upload_pkg.sh -p '/path/pkg_name.son' -u 'http://destination_url:32001/api/v2/packages'\n"
+
+#read the arguments and save them in the variables
+while getopts ":p:u:" option; do
+    case "${option}" in
+        p) PKG=${OPTARG};;
+        u) URL=${OPTARG};;
+        :) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+	*) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
+   	\?) echo "Unknown option: -$OPTARG" >&2; exit 1;;
+    esac
+done
+
+if [ ! "$PKG" ] || [ ! "$URL" ]
+then
+    echo $usage
+    exit 1
+fi
 
 
-resp=$(curl -qSfsw '\n%{http_code}' -d '{"username":"sonata","password":"1234"}' http://sp.int3.sonata-nfv.eu:32001/api/v2/sessions)
-echo "User sonata logged in: $resp"
-token=$(echo $resp | awk '{json=$1 FS $2 FS $3; print json}' | python -mjson.tool | grep "access_token" | awk -F ':[ \t]*' '{print $2}' | sed 's/,//g' | sed 's/"//g')
-echo "TOKEN="$token 
+#echo "el API URL" $URL
+
+LOGIN_URI=$(echo $URL | cut -d':' -f1-2)
+#echo "url cut "$LOGIN_URI
+
+LOGIN_URL="$LOGIN_URI:32001/api/v2/sessions"
+#echo "login url: $LOGIN_URL"
+#illo="http://sp.int3.sonata-nfv.eu:32001/api/v2/sessions"
+#echo illo: $illo
+#echo logu: $LOGIN_URL
+#echo
+
+#token=$(sh get_token.sh $LOGIN_URL)
+#echo "\033[33;32mer token:\n $token"
 
 
 
+echo
+echo These are your options:
+echo
+#echo Package: $PKG
+#echo API URL: $URL
+echo Package: "\033[33;33m $PKG"
+echo "\033[33;37m"
+echo API URL: "\033[33;33m $URL"
+echo
+echo "\033[33;37mPress Enter to continue or CTRL+C to cancel "
+read enter
+echo "\033[33;32m...Retrieving login data and Reading Package Content..."
+echo 
 
-comilla="\""
 
-#Result=$(curl -v -i -X POST -H "Authorization:Bearer $token" -F ""package=@/media/sf_compartida/sonata-empty-service.son"" ""http://sp.int3.sonata-nfv.eu:32001/api/v2/packages"")
+resp=$(curl -qSfsw '\n%{http_code}' -d '{"username":"sonata","password":"1234"}' ""$LOGIN_URL"")
+token=$(printf "$resp" | grep -Po 'access_token":"\K[^"]+')
+
+
+echo "\033[33;37m"
+echo "Done"
+sleep 2
+echo
+echo This is your TOKEN: "\033[33;33m$token "
+echo
+echo
+echo "\033[33;32m...Uploading package..."
+echo "\033[33;37m"
+sleep 2
 
 Result=$(curl -v -i -X POST -H "Authorization:Bearer $token" -F ""package=@$PKG"" ""$URL"")
 
@@ -24,32 +80,43 @@ created_at="created_at"
 if echo "$Result" | grep "$created_at"
 then
   echo
-  echo "Package uploaded correctly"
+  echo "\033[33;33mPackage uploaded correctly"
   echo
-  echo "Uploaded Package info:"
+#  echo "$Result" 
+uuid=$(printf "$Result" | grep -Po 'uuid":"\K[^"]+')
+#echo "UUID doble="$uuid 
+UUID_FINAL=$(echo $uuid| cut -d' ' -f 1)
+echo "\033[33;37m"
+echo "UUID = \033[33;32m $UUID_FINAL"
+son_package_uuid=$(echo $uuid| cut -d' ' -f 2)
+echo "\033[33;37m" 
+echo "SON PACKAGE UUID = \033[33;32m $son_package_uuid"  
+echo "\033[33;37m"
+vendor=$(printf "$Result" | grep -Po 'vendor":"\K[^"]+')
+echo "VENDOR = \033[33;32m $vendor" 
+echo "\033[33;37m"
+status=$(printf "$Result" | grep -Po 'status":"\K[^"]+')
+echo "STATUS = \033[33;32m $status" 
+
+
   echo
-  echo "$Result"
-  echo
-  echo "Message sended to the Message Broker"
+  echo "\033[33;33mMessage sended to the Message Broker"
   MSG=$(tavern-ci mqqt.yml)
   echo 
   echo
 else
   echo
-  echo "Error uploading the package. Check the description"
   echo
-  echo "ERROR Description:" 
+  echo "ERROR UPLOADING THE PACKAGE:"
   echo
-  echo "$Result"
-  echo 
+  echo "\033[33;33m $Result"
+  echo "\033[33;37m"
   echo
 fi
 
-
-echo
 #echo "showing result:"
 #echo $Result
-echo
+
 
 
 
